a1a3c3119d762633863890ace215fcc8d5ba9236
JomiJoestar <yomairapardopajaro@gmail.com>
2026-05-04 12:05:36 -0500

    Aprendiendo metodo .upper y .lower

---
+++ Metodos/Metodos_cadenas.py
@@ -0,0 +1,24 @@
+cadena1 = "Hola soy Jomi"
+cadena2 = "Aprendiendo metodos en python"
+
+#lista de herramientas basicas para trabajar con texto en python
+#DIR:(funcion) Muestra todo que podemos hacer con un objeto en especifico, sea texto, numero, lista... para cada objeto hay cosas diferentes que podemos utilizar
+#print(dir("texto"))
+#print(dir(7))
+#print(dir(["texto"]))
+#print(dir(("texto")))
+
+
+#Metodos en python
+# Dato.ELMETODO() siempre seguido de los parentesis
+
+#convierte a mayusculas
+mayusculas = cadena1.upper()
+print(mayusculas)
+
+#convierte a minisculas
+minusculas = cadena1.lower()
+print(minusculas)
+
+
+

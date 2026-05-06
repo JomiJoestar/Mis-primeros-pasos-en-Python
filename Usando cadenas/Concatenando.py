e8a0a2bc44b60fc7080273d369cdcbb6880517f8
JomiJoestar <JomiJoestar@users.noreply.github.com>
2026-05-06 13:13:13 -0500

    Agrega ejemplo de concatenacion

---
+++ Usando cadenas/Concatenando.py
@@ -0,0 +1,11 @@
+
+#CONCATENANDO CON + 
+print("Hola" + "Mundo") #Concatenacion de cadenas, se unen las dos cadenas sin espacio entre ellas
+
+#METODO CLASICO: OPERADOR +
+
+saludo = "Hola"
+nombre = "Jomi"
+
+print(saludo + " " + nombre) #Concatenacion de cadenas con espacio entre ellas
+
